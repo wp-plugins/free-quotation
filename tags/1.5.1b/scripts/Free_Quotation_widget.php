51765c705291017fc6cd68b0550785e2ea0d5767
kris_IV <kris_IV@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-10 09:35:39 +0000

    tags 1.5.1b
    git-svn-id: https://plugins.svn.wordpress.org/free-quotation@854641 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tags/1.5.1b/scripts/Free_Quotation_widget.php
@@ -0,0 +1,82 @@
+<?php
+global $wpdb;
+global $today_date;
+global $today_week_no;
+$table_name = $wpdb->prefix . 'free_quotation_kris_IV';
+global $wikiuotation;
+
+if ($options['option1']=='1'){
+$Free_Quotation_table = 
+	"
+	SELECT * 
+	FROM $table_name 
+	WHERE display_date='$today_date' 
+	ORDER BY RAND() 
+	LIMIT 1;
+	";
+} elseif ($options['option1']=='5'){
+$Free_Quotation_table = 
+	"
+	SELECT * 
+	FROM $table_name 
+	WHERE week_no='$today_week_no' 
+	ORDER BY RAND() 
+	LIMIT 1;
+	";
+}
+	$result = mysql_query($Free_Quotation_table);
+	
+if ($options['option1']=='1' || $options['option1']=='5') {	
+	//Use only Free_Quotation (if doesn't have it - use standard quotation)
+	if ($row = mysql_fetch_array($result)) { 
+		$quotation = $row['quotation'];
+		$author = $row['author'];
+	} else {
+	$quotation = $options['tekst1'];
+	$author =  $options['tekst2'];
+	}
+} elseif ($options['option1']=='2') {
+	//Use wiqiquotes if dosn't have normal quotes
+	if ($row = mysql_fetch_array($result)) { 
+		$quotation = $row['quotation'];
+		$author = $row['author'];
+	} else {
+		if ($options['option2']=='en') {
+			require(dirname(__FILE__)."/QuotationSystems/WikiquoteEN.php");
+		} elseif ($options['option2']=='de') {
+			require(dirname(__FILE__)."/QuotationSystems/WikiquoteDE.php");
+		} elseif ($options['option2']=='es') {
+			require(dirname(__FILE__)."/QuotationSystems/WikiquoteES.php");
+		} elseif ($options['option2']=='ru') {
+			require(dirname(__FILE__)."/QuotationSystems/WikiquoteRU.php");
+		} elseif ($options['option2']=='pl') {
+			require(dirname(__FILE__)."/QuotationSystems/WikicytatyPL.php");
+		}
+	}
+} elseif ($options['option1']=='3') {	
+	//Use QikiQuote always
+	if ($options['option2']=='en') {
+		require(dirname(__FILE__)."/QuotationSystems/WikiquoteEN.php");
+	} elseif ($options['option2']=='de') {
+		require(dirname(__FILE__)."/QuotationSystems/WikiquoteDE.php");
+	} elseif ($options['option2']=='es') {
+		require(dirname(__FILE__)."/QuotationSystems/WikiquoteES.php");
+	} elseif ($options['option2']=='ru') {
+		require(dirname(__FILE__)."/QuotationSystems/WikiquoteRU.php");
+	} elseif ($options['option2']=='pl') {
+		require(dirname(__FILE__)."/QuotationSystems/WikicytatyPL.php");
+	}
+	
+} elseif ($options['option1']=='4') {	
+	$quotation = $options['tekst1'];
+	$author =  $options['tekst2'];
+	
+}
+?>  <?php
+	
+	echo '<div class="Free_Quotation_quotation">';?><?php if (isset($options['option4'])) {if ($options['option4']==null){ } else { echo $options['tekst3'];}};?><?php echo $quotation;?><?php if (isset($options['option4'])) {if ($options['option4']==null){ } else { echo $options['tekst4'];}};?><?php echo '</div>';
+	echo '<div class="Free_Quotation_author">' . $author . '</div>';
+?> <?php
+
+
+?>
