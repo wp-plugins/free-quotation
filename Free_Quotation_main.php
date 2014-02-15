0e0e76cbf7de094adf872a9abe8023a9d2d7156d
kris_IV <kris_IV@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-15 14:12:38 +0000

    error
    git-svn-id: https://plugins.svn.wordpress.org/free-quotation/tags/1.5.2@858458 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- Free_Quotation_main.php
+++ Free_Quotation_main.php
@@ -3,7 +3,7 @@
 	Plugin Name: Free Quotation by KRIS_IV
 	Description: Quotation displayer for any WordPress page
 	Author: Krzysztof Kubiak
-	Version: v1.5.2
+	Version: 1.5.2
 	Author URI: http://my-motivator.pl/Free_Quotation
 	License: GPLv2
 	License URI: http://www.gnu.org/licenses/gpl-2.0.html
