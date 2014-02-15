e24ed8a3a37bd4ee54c8e275389fadc04bb3ca98
kris_IV <kris_IV@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-15 12:21:31 +0000

    1.5.4-beta
    git-svn-id: https://plugins.svn.wordpress.org/free-quotation@858401 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- trunk/Free_Quotation_main.php
+++ trunk/Free_Quotation_main.php
@@ -3,14 +3,14 @@
 	Plugin Name: Free Quotation by KRIS_IV
 	Description: Quotation displayer for any WordPress page
 	Author: Krzysztof Kubiak
-	Version: 1.5.3-beta
+	Version: 1.5.4-beta
 	Author URI: http://my-motivator.pl/Free_Quotation
 	License: GPLv2
 	License URI: http://www.gnu.org/licenses/gpl-2.0.html
 */
 global $wpdb;
 global $Free_Quotation_version;
-$Free_Quotation_version = "1.5.3-beta";
+$Free_Quotation_version = "1.5.4-beta";
 global $today_date;
 $today_date = date('Y-m-d');
 global $today_week_no;
