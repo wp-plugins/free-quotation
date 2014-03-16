950227e42be5f2886d61e5febd80ffed37916404
kris_IV <kris_IV@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-16 16:32:58 +0000

    New version of plugin with additional customizing option.
    git-svn-id: https://plugins.svn.wordpress.org/free-quotation@876268 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- trunk/scripts/Free_Quotation_admin_settings.php
+++ trunk/scripts/Free_Quotation_admin_settings.php
@@ -9,71 +9,167 @@ $table_name = $wpdb->prefix . 'free_quotation_kris_IV';
 <div class="wrap">	
 	 <h2><div class="Free_Quotation_header"></div>Free Quotation <?php echo $Free_Quotation_version; ?><a class="add-new-h2" href="admin.php?page=fq_menu_page">Free Quotation list</a></h2></h2>
 	<br><h2>Settings page</h2>
-	
+
+<script>
+	jQuery(document).ready( function($){
+		$( "#set_page" ).tabs();
+	});
+</script>
 		<form method="post" action="options.php">
 			<?php settings_fields('Free_Quotation_settings_filed'); ?>
 			<?php $options = get_option('Free_Quotation_options'); ?>
-			<table class="form-table">
-				<tr>
-					<th scope="row">Permit to write date (in Add Free Quotation page)</th>
-					<td>
-						<input name="Free_Quotation_options[option3]" type="checkbox" value="1" <?php if(isset($options['option3'])){checked($options['option3'], 1);}?> /><br>
-					</td>
-				</tr>
-				<tr>
-					<th scope="row">Start and end quotation with special characters</th>
-					<td>
-						<input name="Free_Quotation_options[option4]" id="quotationsign" type="checkbox" value="1" <?php if(isset($options['option4'])){checked($options['option4'], 1);} ?> /><br>
-					</td>
-				</tr>
+			<div id="set_page">
+				<ul>
+					<li><a href="#set_page-1">General</a></li>
+					<li><a href="#set_page-2">Viasual</a></li>
+				</ul>
+				<div id="set_page-1">
+					<table class="form-table">
+						<tr>
+							<th scope="row">Permit to write date (in Add Free Quotation page)</th>
+							<td>
+								<input name="Free_Quotation_options[option3]" type="checkbox" value="1" <?php if(isset($options['option3'])){checked($options['option3'], 1);}?> /><br>
+							</td>
+						</tr>
+						<tr>
+							<th scope="row">Start and end quotation with special characters</th>
+							<td>
+								<input name="Free_Quotation_options[option4]" id="quotationsign" type="checkbox" value="1" <?php if(isset($options['option4'])){checked($options['option4'], 1);} ?> /><br>
+							</td>
+						</tr>
+					
+						<tr style="background:#ccc; width: 900px; <?php if ($options['option4']==null){ echo 'display:none;';} else {}?>">
+							<th scope="row">Start character</th>
+							<td>
+								<input name="Free_Quotation_options[tekst3]" type="text" id="activator1" <?php if (isset($options['option4'])) {} else {echo 'disabled=""';};?> value="<?php if(isset($options['tekst3'])){echo htmlentities($options['tekst3']);}else{};?>" maxlength="1" size="1"></input>
+							</td>
+						</tr>	
+						<tr style="background:#ccc; width: 900px; <?php if ($options['option4']==null){ echo 'display:none;';} else {}?>">
+							<th scope="row">End character</th>
+							<td>
+								<input name="Free_Quotation_options[tekst4]" type="text" id="activator2" <?php if (isset($options['option4'])) {} else {echo 'disabled=""';};?> value="<?php if(isset($options['tekst3'])){echo htmlentities($options['tekst3']);}else{};?>" maxlength="1" size="1"></input>
+							</td>
+						</tr>
+					<?php ?>
+						<tr>
+							<th scope="row">Type of quotation display:</th>
+							<td>
+								<input type="radio" name="Free_Quotation_options[option1]" value="1" <?php checked('1', $options['option1']); ?> />Use only Free_Quotation - display by day</br>
+								<input type="radio" name="Free_Quotation_options[option1]" value="5" <?php checked('5', $options['option1']); ?> />Use only Free_Quotation - display by week number</br>
+								<input type="radio" name="Free_Quotation_options[option1]" value="6" <?php checked('6', $options['option1']); ?> />Use only Free_Quotation - random quotation for specific day</br>
+								<input type="radio" name="Free_Quotation_options[option1]" value="2" <?php checked('2', $options['option1']); ?> />Use Wikiquote if you doesn't have FQ for display<br>
+								<input type="radio" name="Free_Quotation_options[option1]" value="3" <?php checked('3', $options['option1']); ?> />Use Wikiquote always for quotations displaying<br>
+								<input type="radio" name="Free_Quotation_options[option1]" value="4" <?php checked('4', $options['option1']); ?> />Use one standard quotation (always active if Free_Quotation doesn't have quotation for displaying)<br>
+							</td>
+						</tr>
+						<tr>
+							<th scope="row">Choose Wikiquote system:</th>
+							<td><select name="Free_Quotation_options[option2]">
+								<option <?php if($options['option2']=="en" ){ echo "selected"; } ?> value="en">Wikiquote [en] (Quote of the day)</option>
+								<option <?php if($options['option2']=="de"){ echo "selected";} ?> value="de">Wikiquote [de] (Zitat der Woche)</option>
+								<option <?php if($options['option2']=="es"){ echo "selected";} ?> value="es">Wikiquote [es] (Cita del día)</option>
+								<option <?php if($options['option2']=="ru"){ echo "selected";} ?> value="ru">Wikiquote [ru] (Избранная цитата)</option>
+								<option <?php if($options['option2']=="pl"){ echo "selected";} ?> value="pl">Wikiquote [pl] (Cytat dnia)</option>
+								</select>
+							</td>
+						</tr>
+						<tr style="background:#ccc; width: 900px;">
+							<th scope="row">Standard quotation</th>
+								<td><textarea name="Free_Quotation_options[tekst1]" type="text" cols="80" rows="2"><?php echo $options['tekst1']; ?></textarea></td>
+						</tr>			
+						<tr style="background:#ccc;">
+							<th scope="row">Quotation author</th>
+							<td>
+								<input name="Free_Quotation_options[tekst2]" type="input" value="<?php echo $options['tekst2']; ?>"></input>
+							</td>
+						</tr>
+						
+					</table>
+				</div>
+				<div id="set_page-2">
+					<table class="form-table">
+						<tr>
+							<th scope="row">Do you want use your own style?</th>
+							<td>
+								<input name="Free_Quotation_options[option5]" type="checkbox" value="1" <?php if(isset($options['option5'])){checked($options['option5'], 1);}?> /><br>
+							</td>
+						</tr>
+						<tr>
+							<th scope="row">Header size:</th>
+							<td>
+								<input name="Free_Quotation_options[tekst6]" type="text" id="activator1" value="<?php if(isset($options['tekst6'])){echo htmlentities($options['tekst6']);}else{};?>" maxlength="2" size="2"></input>px
+							</td>
+						</tr>
+						<tr>
+							<th scope="row">Header font:</th>
+							<td>
+								<input name="Free_Quotation_options[tekst7]" type="text" id="activator1" value="<?php if(isset($options['tekst7'])){echo htmlentities($options['tekst7']);}else{};?>" maxlength="20" size="20"></input>
+							</td>
+						</tr>
+						<tr>
+							<th scope="row">Header bold:</th>
+							<td>
+								<input name="Free_Quotation_options[option6]" type="checkbox" value="1" <?php if(isset($options['option6'])){checked($options['option6'], 1);}?> /><br>
+							</td>
+						</tr>
+						<tr>
+							<th scope="row">Header italic:</th>
+							<td>
+								<input name="Free_Quotation_options[option7]" type="checkbox" value="1" <?php if(isset($options['option7'])){checked($options['option7'], 1);}?> /><br>
+							</td>
+						</tr>
+						<tr style="background:#ccc; width: 900px;">
+							<th scope="row">Body size:</th>
+							<td>
+								<input name="Free_Quotation_options[tekst8]" type="text" id="activator1" value="<?php if(isset($options['tekst8'])){echo htmlentities($options['tekst8']);}else{};?>" maxlength="2" size="2"></input>px
+							</td>
+						</tr>
+						<tr style="background:#ccc; width: 900px;">
+							<th scope="row">Body font:</th>
+							<td>
+								<input name="Free_Quotation_options[tekst9]" type="text" id="activator1" value="<?php if(isset($options['tekst9'])){echo htmlentities($options['tekst9']);}else{};?>" maxlength="20" size="20"></input>
+							</td>
+						</tr>
+						<tr style="background:#ccc; width: 900px;">
+							<th scope="row">Body bold:</th>
+							<td>
+								<input name="Free_Quotation_options[option8]" type="checkbox" value="1" <?php if(isset($options['option8'])){checked($options['option8'], 1);}?> /><br>
+							</td>
+						</tr>
+						<tr style="background:#ccc; width: 900px;">
+							<th scope="row">Body italic:</th>
+							<td>
+								<input name="Free_Quotation_options[option9]" type="checkbox" value="1" <?php if(isset($options['option9'])){checked($options['option9'], 1);}?> /><br>
+							</td>
+						</tr>
+						<tr>
+							<th scope="row">Signature size:</th>
+							<td>
+								<input name="Free_Quotation_options[tekst10]" type="text" id="activator1" value="<?php if(isset($options['tekst10'])){echo htmlentities($options['tekst10']);}else{};?>" maxlength="2" size="2"></input>px
+							</td>
+						</tr>
+						<tr>
+							<th scope="row">Signature font:</th>
+							<td>
+								<input name="Free_Quotation_options[tekst11]" type="text" id="activator1" value="<?php if(isset($options['tekst11'])){echo htmlentities($options['tekst11']);}else{};?>" maxlength="20" size="20"></input>
+							</td>
+						</tr>
+						<tr>
+							<th scope="row">Signature bold:</th>
+							<td>
+								<input name="Free_Quotation_options[option10]" type="checkbox" value="1" <?php if(isset($options['option10'])){checked($options['option10'], 1);}?> /><br>
+							</td>
+						</tr>
+						<tr>
+							<th scope="row">Signature italic:</th>
+							<td>
+								<input name="Free_Quotation_options[option11]" type="checkbox" value="1" <?php if(isset($options['option11'])){checked($options['option11'], 1);}?> /><br>
+							</td>
+						</tr>
+					</table>
+				</div>
+			</div>
 			
-				<tr style="background:#ccc; width: 900px; <?php if ($options['option4']==null){ echo 'display:none;';} else {}?>">
-					<th scope="row">Start character</th>
-					<td>
-						<input name="Free_Quotation_options[tekst3]" type="text" id="activator1" <?php if (isset($options['option4'])) {} else {echo 'disabled=""';};?> value="<?php if(isset($options['tekst3'])){echo htmlentities($options['tekst3']);}else{};?>" maxlength="1" size="1"></input>
-					</td>
-				</tr>	
-				<tr style="background:#ccc; width: 900px; <?php if ($options['option4']==null){ echo 'display:none;';} else {}?>">
-					<th scope="row">End character</th>
-					<td>
-						<input name="Free_Quotation_options[tekst4]" type="text" id="activator2" <?php if (isset($options['option4'])) {} else {echo 'disabled=""';};?> value="<?php if(isset($options['tekst3'])){echo htmlentities($options['tekst3']);}else{};?>" maxlength="1" size="1"></input>
-					</td>
-				</tr>
-			<?php ?>
-				<tr>
-					<th scope="row">Type of quotation display:</th>
-					<td>
-						<input type="radio" name="Free_Quotation_options[option1]" value="1" <?php checked('1', $options['option1']); ?> />Use only Free_Quotation - display by day</br>
-						<input type="radio" name="Free_Quotation_options[option1]" value="5" <?php checked('5', $options['option1']); ?> />Use only Free_Quotation - display by week number</br>
-						<input type="radio" name="Free_Quotation_options[option1]" value="6" <?php checked('6', $options['option1']); ?> />Use only Free_Quotation - random quotation for specific day</br>
-						<input type="radio" name="Free_Quotation_options[option1]" value="2" <?php checked('2', $options['option1']); ?> />Use Wikiquote if you doesn't have FQ for display<br>
-						<input type="radio" name="Free_Quotation_options[option1]" value="3" <?php checked('3', $options['option1']); ?> />Use Wikiquote always for quotations displaying<br>
-						<input type="radio" name="Free_Quotation_options[option1]" value="4" <?php checked('4', $options['option1']); ?> />Use one standard quotation (always active if Free_Quotation doesn't have quotation for displaying)<br>
-					</td>
-				</tr>
-				<tr>
-					<th scope="row">Choose Wikiquote system:</th>
-					<td><select name="Free_Quotation_options[option2]">
-						<option <?php if($options['option2']=="en" ){ echo "selected"; } ?> value="en">Wikiquote [en] (Quote of the day)</option>
-						<option <?php if($options['option2']=="de"){ echo "selected";} ?> value="de">Wikiquote [de] (Zitat der Woche)</option>
-						<option <?php if($options['option2']=="es"){ echo "selected";} ?> value="es">Wikiquote [es] (Cita del día)</option>
-						<option <?php if($options['option2']=="ru"){ echo "selected";} ?> value="ru">Wikiquote [ru] (Избранная цитата)</option>
-						<option <?php if($options['option2']=="pl"){ echo "selected";} ?> value="pl">Wikiquote [pl] (Cytat dnia)</option>
-						</select>
-					</td>
-				</tr>
-				<tr style="background:#ccc; width: 900px;">
-					<th scope="row">Standard quotation</th>
-						<td><textarea name="Free_Quotation_options[tekst1]" type="text" cols="80" rows="2"><?php echo $options['tekst1']; ?></textarea></td>
-				</tr>			
-				<tr style="background:#ccc;">
-					<th scope="row">Quotation author</th>
-					<td>
-						<input name="Free_Quotation_options[tekst2]" type="input" value="<?php echo $options['tekst2']; ?>"></input>
-					</td>
-				</tr>
-				
-			</table>
 			<p class="submit">
 			<input type="submit" class="button-primary" value="<?php _e('Save') ?>" />
 			</p>
@@ -99,6 +195,10 @@ jQuery(document).ready( function($){
 		<li><a href="#tabs-7">ver. 1.0</a></li>
 	  </ul>
 	  <div id="tabs-1">
+			2.0.4
+			<ul>
+				<li>Now you can style your widget quotation (global to all widgets)</li>
+			</ul>
 			2.0.3
 			<ul>
 				<li>Add new quotation to the group from selected box</li>
